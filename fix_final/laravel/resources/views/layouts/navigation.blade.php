df949295ee2f0fa2248f9cef021e3f4bc0dacc92
naifah13 <sitinaifah473@gmail.com>
2026-05-11 12:23:10 +0700

    modif navbar

--- fix_final/laravel/resources/views/layouts/navigation.blade.php
+++ fix_final/laravel/resources/views/layouts/navigation.blade.php
@@ -57,6 +57,12 @@
             <input type="text" placeholder="Search..." class="pl-10 pr-4 py-2 bg-gray-100 border-none rounded-full text-sm w-40 focus:ring-2 focus:ring-pink-100 focus:bg-white transition-all">
         </div>
 
+        <button class="text-gray-500 hover:text-black transition-colors relative">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"></path>
+            </svg>
+        </button>
+
         <button class="text-gray-500 hover:text-black transition-colors relative group focus:outline-none">
     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
