ba9f598e059530cb2afbfc28f7da57c59666abaf
tabhitaksilitonga <tabhitasilitonga@gmail.com>
2026-05-15 17:00:58 +0700

    editadmin(4)

--- fix_final/laravel/app/Providers/AppServiceProvider.php
+++ fix_final/laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 use App\Models\Category;
 use Illuminate\Support\Facades\View;
 
@@ -22,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
 {
     View::share('categories', Category::all());
+    Paginator::useBootstrap();
 }
 }
