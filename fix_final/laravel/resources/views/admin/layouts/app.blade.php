2c969aaa87072fb62b6d8754c435cb85a8563095
tabhitaksilitonga <tabhitasilitonga@gmail.com>
2026-05-16 17:49:41 +0700

    adminlagi

--- fix_final/laravel/resources/views/admin/layouts/app.blade.php
+++ fix_final/laravel/resources/views/admin/layouts/app.blade.php
@@ -32,7 +32,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-palette"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">Admin Panel</div>
+                <div class="sidebar-brand-text mx-3">Dribbble's Admin</div>
             </a>
 
             <hr class="sidebar-divider my-0">
